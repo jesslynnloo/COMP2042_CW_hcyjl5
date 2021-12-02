b1126789983c9da6b5ae0e1f2b9f72c5d5d638ab
jesslynnloo <jesslynnlooshilin@gmail.com>
2021-12-02 18:02:13 +0800

    Refactor GameBoard class to adhere to MVC pattern.

---
+++ src/test/view/GameView.java
@@ -0,0 +1,301 @@
+package test.view;
+
+import test.controller.GameController;
+import test.model.Ball;
+import test.model.Brick;
+import test.model.Player;
+import test.model.Wall;
+import test.view.DebugConsole;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.font.FontRenderContext;
+
+
+
+public class GameView extends JComponent implements KeyListener,MouseListener,MouseMotionListener {
+
+    private static final String CONTINUE = "Continue";
+    private static final String RESTART = "Restart";
+    private static final String EXIT = "Exit";
+    private static final String PAUSE = "Pause Menu";
+    private static final int TEXT_SIZE = 30;
+    private static final Color MENU_COLOR = new Color(0,255,0);
+
+
+    private static final int DEF_WIDTH = 600;
+    private static final int DEF_HEIGHT = 450;
+
+    private static final Color BG_COLOR = Color.WHITE;
+
+    private Wall wall;
+
+    private String message;
+
+    private Font menuFont;
+
+    private Rectangle continueButtonRect;
+    private Rectangle exitButtonRect;
+    private Rectangle restartButtonRect;
+    private int strLen;
+
+    private DebugConsole debugConsole;
+
+    private GameController gameController = new GameController(this);
+
+    public GameView(JFrame owner){
+        super();
+
+        strLen = 0;
+        gameController.setShowPauseMenu(false);
+
+        menuFont = new Font("Monospaced",Font.PLAIN,TEXT_SIZE);
+
+        this.initialize();
+        message = "";
+        wall = new Wall(new Rectangle(0,0,DEF_WIDTH,DEF_HEIGHT),30,3,6/2,new Point(300,430));
+
+        debugConsole = new DebugConsole(owner,wall,gameController);
+        //initialize the first level
+        wall.nextLevel();
+    }
+
+
+
+    private void initialize(){
+        this.setPreferredSize(new Dimension(DEF_WIDTH,DEF_HEIGHT));
+        this.setFocusable(true);
+        this.requestFocusInWindow();
+        this.addKeyListener(this);
+        this.addMouseListener(this);
+        this.addMouseMotionListener(this);
+    }
+
+
+    public void paint(Graphics g){
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        clear(g2d);
+
+        g2d.setColor(Color.BLUE);
+        g2d.drawString(message,250,225);
+
+        drawBall(wall.getBall(),g2d);
+
+        for(Brick b : wall.getBricks())
+            if(!b.isBroken())
+                drawBrick(b,g2d);
+
+        drawPlayer(wall.getPlayer(),g2d);
+
+        if(gameController.isShowPauseMenu())
+            drawMenu(g2d);
+
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    private void clear(Graphics2D g2d){
+        Color tmp = g2d.getColor();
+        g2d.setColor(BG_COLOR);
+        g2d.fillRect(0,0,getWidth(),getHeight());
+        g2d.setColor(tmp);
+    }
+
+    private void drawBrick(Brick brick,Graphics2D g2d){
+        Color tmp = g2d.getColor();
+
+        g2d.setColor(brick.getInnerColor());
+        g2d.fill(brick.getBrick());
+
+        g2d.setColor(brick.getBorderColor());
+        g2d.draw(brick.getBrick());
+
+
+        g2d.setColor(tmp);
+    }
+
+    private void drawBall(Ball ball, Graphics2D g2d){
+        Color tmp = g2d.getColor();
+
+        Shape s = ball.getBallFace();
+
+        g2d.setColor(ball.getInnerColor());
+        g2d.fill(s);
+
+        g2d.setColor(ball.getBorderColor());
+        g2d.draw(s);
+
+        g2d.setColor(tmp);
+    }
+
+    private void drawPlayer(Player p, Graphics2D g2d){
+        Color tmp = g2d.getColor();
+
+        Shape s = p.getPlayerFace();
+        g2d.setColor(Player.INNER_COLOR);
+        g2d.fill(s);
+
+        g2d.setColor(Player.BORDER_COLOR);
+        g2d.draw(s);
+
+        g2d.setColor(tmp);
+    }
+
+    private void drawMenu(Graphics2D g2d){
+        obscureGameBoard(g2d);
+        drawPauseMenu(g2d);
+    }
+
+    private void obscureGameBoard(Graphics2D g2d){
+
+        Composite tmp = g2d.getComposite();
+        Color tmpColor = g2d.getColor();
+
+        AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER,0.55f);
+        g2d.setComposite(ac);
+
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect(0,0,DEF_WIDTH,DEF_HEIGHT);
+
+        g2d.setComposite(tmp);
+        g2d.setColor(tmpColor);
+    }
+
+    private void drawPauseMenu(Graphics2D g2d){
+        Font tmpFont = g2d.getFont();
+        Color tmpColor = g2d.getColor();
+
+
+        g2d.setFont(menuFont);
+        g2d.setColor(MENU_COLOR);
+
+        if(strLen == 0){
+            FontRenderContext frc = g2d.getFontRenderContext();
+            strLen = menuFont.getStringBounds(PAUSE,frc).getBounds().width;
+        }
+
+        int x = (this.getWidth() - strLen) / 2;
+        int y = this.getHeight() / 10;
+
+        g2d.drawString(PAUSE,x,y);
+
+        x = this.getWidth() / 8;
+        y = this.getHeight() / 4;
+
+
+        if(continueButtonRect == null){
+            FontRenderContext frc = g2d.getFontRenderContext();
+            continueButtonRect = menuFont.getStringBounds(CONTINUE,frc).getBounds();
+            continueButtonRect.setLocation(x,y-continueButtonRect.height);
+        }
+
+        g2d.drawString(CONTINUE,x,y);
+
+        y *= 2;
+
+        if(restartButtonRect == null){
+            restartButtonRect = (Rectangle) continueButtonRect.clone();
+            restartButtonRect.setLocation(x,y-restartButtonRect.height);
+        }
+
+        g2d.drawString(RESTART,x,y);
+
+        y *= 3.0/2;
+
+        if(exitButtonRect == null){
+            exitButtonRect = (Rectangle) continueButtonRect.clone();
+            exitButtonRect.setLocation(x,y-exitButtonRect.height);
+        }
+
+        g2d.drawString(EXIT,x,y);
+
+
+
+        g2d.setFont(tmpFont);
+        g2d.setColor(tmpColor);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+        gameController.checkKeyPressed(keyEvent);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+        gameController.checkKeyReleased(keyEvent);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+        gameController.checkMouseClicked(mouseEvent);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
+        gameController.checkMouseMoved(mouseEvent);
+    }
+
+    public Wall getWall() {
+        return wall;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Rectangle getContinueButtonRect() {
+        return continueButtonRect;
+    }
+
+    public Rectangle getExitButtonRect() {
+        return exitButtonRect;
+    }
+
+    public Rectangle getRestartButtonRect() {
+        return restartButtonRect;
+    }
+
+    public DebugConsole getDebugConsole() {
+        return debugConsole;
+    }
+
+    public void setHandCursor () {
+        this.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+    }
+
+    public void setDefaultCursor () {
+        this.setCursor(Cursor.getDefaultCursor());
+    }
+
+}
