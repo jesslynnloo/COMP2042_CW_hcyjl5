7b98699a8b12ebdcb466a3071217055ac672cd1f
jesslynnloo <jesslynnlooshilin@gmail.com>
2021-12-07 23:19:31 +0800

    Add javadoc comments for PlayerController class.

--- src/test/controller/PlayerController.java
+++ src/test/controller/PlayerController.java
@@ -2,16 +2,28 @@ package test.controller;
 
 import test.model.Player;
 
+/**
+ * This is the PlayerController class.
+ */
 public class PlayerController {
     private Player player;
 
 
+    /**
+     * Class constructor.
+     * @param player The Player object.
+     */
     public PlayerController(Player player) {
         this.player = player;
     }
 
-    public void adjustPlayer(int extendedWidth, int height) {
-        player.updatePlayerFace(extendedWidth,height);
+    /**
+     * Call the updatePlayerFace method in the player class to make player face based on the width and height passed.
+     * @param width The new width that is used to update the player face.
+     * @param height The new height that is used to update the player face.
+     */
+    public void adjustPlayer(int width, int height) {
+        player.updatePlayerFace(width,height);
     }
 
 }
