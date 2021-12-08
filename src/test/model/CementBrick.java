d1120a001b870c076ff10f7cb87cca712a1aab39
jesslynnloo <jesslynnlooshilin@gmail.com>
2021-12-08 09:33:46 +0800

    Adding javadoc comments for CementBrick class.

--- src/test/model/CementBrick.java
+++ src/test/model/CementBrick.java
@@ -7,6 +7,9 @@ import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
 
 
+/**
+ * This is the CementBrick class which extends Brick class.
+ */
 public class CementBrick extends Brick {
 
 
@@ -20,6 +23,13 @@ public class CementBrick extends Brick {
     private Shape brickFace;
     private HighScoreController highScoreController;
 
+    /**
+     * Class constructor.
+     * Initialize a Crack object.
+     * Initialize a HighScoreController object.
+     * @param point The position of the cement brick.
+     * @param size The size of the cement brick.
+     */
     public CementBrick(Point point, Dimension size){
         super(NAME,point,size,DEF_BORDER,DEF_INNER,CEMENT_STRENGTH);
         crack = new Crack(this, DEF_CRACK_DEPTH,DEF_STEPS);
@@ -27,11 +37,30 @@ public class CementBrick extends Brick {
         highScoreController = new HighScoreController();
     }
 
+    /**
+     * Make the shape of the brick.
+     * @param pos  The position of the brick.
+     * @param size The size of the brick.
+     * @return The cement brick shape.
+     */
     @Override
     protected Shape makeBrickFace(Point pos, Dimension size) {
         return new Rectangle(pos,size);
     }
 
+    /**
+     * Perform actions when the ball is in impact with the cement brick.
+     * If the cement brick is broken, return false.
+     * Call the impact() method from the Brick class, minus the strength of the cement brick by 1
+     * and check if the cement brick is broken.
+     * If the cement brick is not broken, make crack on the cement brick and draw the crack on the cement brick,
+     * then return false.
+     * If the cement brick is broken, then update the score by adding the score with the score for destroying a cement brick,
+     * then return true.
+     * @param point The position of the ball impact with the brick.
+     * @param dir   An int value that represents the direction of brick that the ball impact with.
+     * @return A boolean value which represents if the brick is broken.
+     */
     @Override
     public boolean setImpact(Point2D point, int dir) {
         if(super.isBroken())
@@ -47,11 +76,18 @@ public class CementBrick extends Brick {
     }
 
 
+    /**
+     * Get the value of the brickFace.
+     * @return A Shape value of brickFace.
+     */
     @Override
     public Shape getBrick() {
         return brickFace;
     }
 
+    /**
+     * Draw crack on the brick.
+     */
     private void updateBrick(){
         if(!super.isBroken()){
             GeneralPath gp = crack.draw();
@@ -60,6 +96,11 @@ public class CementBrick extends Brick {
         }
     }
 
+    /**
+     * Repair the brick to its original state.
+     * Set the broken variable to false and set the strength of the brick to its full strength.
+     * Reset the crack and brickFace.
+     */
     public void repair(){
         super.repair();
         crack.reset();
