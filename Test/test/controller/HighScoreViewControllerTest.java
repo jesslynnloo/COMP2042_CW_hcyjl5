4747c605ee05cb58fe461f56b112d4b78d17e48f
jesslynnloo <jesslynnlooshilin@gmail.com>
2021-12-11 12:16:39 +0800

    Adding unit tests for HighScoreViewController class

---
+++ Test/test/controller/HighScoreViewControllerTest.java
@@ -0,0 +1,49 @@
+package test.controller;
+
+import org.junit.jupiter.api.Test;
+import test.view.GameFrame;
+import test.view.HighScoreView;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HighScoreViewControllerTest {
+    GameFrame gameFrame = new GameFrame();
+    HighScoreView highScoreView = new HighScoreView(gameFrame);
+    HighScoreViewController highScoreViewController = new HighScoreViewController(highScoreView);
+
+    @Test
+    void isRestartClicked() {
+        highScoreViewController.setRestartClicked(true);
+        assertTrue(highScoreViewController.isRestartClicked());
+    }
+
+    @Test
+    void setRestartClicked() {
+        highScoreViewController.setRestartClicked(true);
+        assertTrue(highScoreViewController.isRestartClicked());
+    }
+
+    @Test
+    void isHomeMenuClicked() {
+        highScoreViewController.setHomeMenuClicked(true);
+        assertTrue(highScoreViewController.isHomeMenuClicked());
+    }
+
+    @Test
+    void setHomeMenuClicked() {
+        highScoreViewController.setHomeMenuClicked(true);
+        assertTrue(highScoreViewController.isHomeMenuClicked());
+    }
+
+    @Test
+    void isExitClicked() {
+        highScoreViewController.setExitClicked(true);
+        assertTrue(highScoreViewController.isExitClicked());
+    }
+
+    @Test
+    void setExitClicked() {
+        highScoreViewController.setExitClicked(true);
+        assertTrue(highScoreViewController.isExitClicked());
+    }
+}
